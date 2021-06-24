5d842b7c3e2a402b18c6fb00cb73211211b6aed7
mbwright <myleswright0518@gmail.com>
2021-06-23 23:16:12 -0400

    Home page done

--- src/App.js
+++ src/App.js
@@ -2,10 +2,74 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import Particles from "react-particles-js";
 
 function App() {
   return (
+    <>
+    <Particles 
+        
+      params={{
+        polygon:{
+          scale: 5,
+          type: "inside",
+          draw:{
+            stroke:{
+              color: "#F9C80E",
+              width: 50
+            }
+          }
+        },
+        particles:{
+          collisions:{
+            enable: true
+          },
+          move: {
+            direction: "none",
+            enable: true,
+            outMode: "bounce",
+            random: false,
+            speed: 3,
+            straight: false,
+          },
+          color:{
+            value: "#FF4365"
+          },
+          shape:{
+            type: "circle"
+          },
+          size:{
+            random: true,
+            value: 8
+          },
+          links:{
+            color: "#F9C80E",
+            width: 2,
+            distance: 150 
+          },
+          line_linked:{
+            shadow:{
+              enable: true,
+              color: "#FF4365",
+              blur: 10
+            }
+          }
+        },
+        number:{
+          value: 80,
+          density: {
+            enable: true, 
+            value_area: 800
+          }
+          
+        }
+      }}
+    
+    />
     <Navbar />
+    <Header />
+    </>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Typed from "react-typed";
+
+
+const Header = () => {
+    return (
+        <div className ="header-wrapper">
+            <div className="main-info">
+                <h1>Hi, I'm Myles!</h1>
+                <Typed
+                    className="typed-text"
+                    strings={["React", "Visualforce", "Apex", "Swift", "RESTful APIs", "Agile Development", "Database Development", "IOS Development"]}
+                    typeSpeed={40}
+                    backSpeed={80}
+                    loop
+                />
+                <a href="#" className = "btn-contact">Contact Me</a>
+            </div>
+            
+        </div>
+    )
+}
+
+export default Header
