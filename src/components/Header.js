6971c99719e018d3d6517821a0de4cffb8f42de5
Myles Wright <myleswright@LAP-G9XXYYV44F3.attlocal.net>
2024-12-11 11:54:00 -0600

    updated info

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,12 +9,12 @@ const Header = () => {
                 <h1>Hi, I'm Myles!</h1>
                 <Typed
                     className="typed-text"
-                    strings={["React", "Visualforce", "Apex", "Swift", "RESTful APIs", "Agile Development", "Database Development", "IOS Development"]}
+                    strings={["React", "Javascript", "Laravel", "PHP", "Magento 2", "Integration Development", "Python", "Frontend Design", "RESTful APIs", "Agile Development", "Database Development", "Full Stack Development"]}
                     typeSpeed={40}
                     backSpeed={80}
                     loop
                 />
-                <p>Nice to see you here! I am a software developer who works on multiple architectures and study areas, including HCI research and development. Feel free to contact me, I'd love to chat!</p>
+                <p>Nice to see you here! I am a full-stack software developer working on multiple architectures, including e-commerce and API integrations. Feel free to contact me, I'd love to chat!</p>
             </div>
             
         </div>
